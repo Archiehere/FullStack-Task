75ea3fdfba6a997c5773817367966550e81cd836
Archiehere <cheeku67@gmail.com>
2023-06-28 18:51:02 +0530

    ADD:Fullstack Task

---
+++ frontend/Translator-Frontend/src/App.jsx
@@ -0,0 +1,91 @@
+import { useState } from 'react'
+// import Button from '@mui/material/Button';
+import LoadingButton from '@mui/lab/LoadingButton';
+import Paper from '@mui/material/Paper';
+import TextField from '@mui/material/TextField';
+import axios from 'axios';
+import './App.css'
+
+function App() {
+
+  
+//create a state variable to store the value of the input field string
+const [inputValue, setInputValue] = useState('')
+//create a state of result to store the result of the api call
+const [result, setResult] = useState('Result will be shown here')
+//loading state
+const [loading, setLoading] = useState(false)
+//audioUrl state
+const [audioUrl, setAudioUrl] = useState('')
+
+function handleapi(){
+  setLoading(true);
+ axios.post('http://localhost:8080/translate',{
+    text: inputValue,
+    target: 'hi'
+  }).then((res)=>{
+
+    setResult(res.data.textdata);
+    console.log(res.data.textdata);
+    let audioData = res.data.audiodata.audioContent.data;
+
+    // Convert the audio data to a Blob
+    let audioBlob = new Blob([new Uint8Array(audioData)], { type: 'audio/wav' });
+
+    // Create a Blob URL
+    let audioUrl = URL.createObjectURL(audioBlob);
+    let audioElement = new Audio(audioUrl);
+    audioElement.play();
+    setAudioUrl(audioUrl);  
+    setLoading(false);
+  }
+  ).catch((err)=>{
+    console.log(err);
+    setLoading(false);
+ } ) 
+}
+  return (
+//create a div with an input field and a button using material ui and return it
+<>
+<div className='container'>
+<h2>Input English Text here!</h2>
+<TextField id="outlined-basic" label="Outlined" variant="outlined" value={inputValue} onChange={(e)=>{
+  setInputValue(e.target.value);
+}}/>
+ <LoadingButton
+          size="small"
+          onClick={handleapi}
+          loading={loading}
+          loadingIndicator="Loading…"
+          variant="outlined"
+        > <span>Fetch data</span>
+        </LoadingButton>
+<h3>Output Hindi Text:-</h3>
+<Paper elevation={10} className='result'> {result} </Paper>
+{/* <PlayArrowIcon style={{type:'button' , color:'white',padding:'0.5rem',background:'black',cursor:'pointer'}} /> */}
+<div>{audioUrl ? 
+<div>
+<audio controls key={audioUrl}>
+  <source src={audioUrl} type="audio/wav" />
+</audio>
+<br />
+<br />
+<a href={audioUrl} download="audio.wav">
+          Download Audio
+        </a>
+</div>
+: null}
+    </div>
+</div>
+</>
+  )  
+}
+
+export default App
+
+
+
+
+
+
+
---
+++ index.js
@@ -0,0 +1,77 @@
+// Imports the Google Cloud client library
+
+const {Translate} = require('@google-cloud/translate').v2;
+const express = require('express');
+const cors=require('cors');
+const rateLimit  = require('express-rate-limit');
+const {errorMiddleware, ErrorHandler } = require('./middlewares/errors');
+const textToSpeech = require('@google-cloud/text-to-speech');
+
+// Import other required libraries
+const fs = require('fs');
+const util = require('util');
+
+
+let PORT=8080;
+
+const translate = new Translate({
+   keyFilename: './apikey.json'
+});
+
+const client = new textToSpeech.TextToSpeechClient({
+    keyFilename: './apikey.json'
+ });
+
+const app = express();
+	app.use(express.json());
+	app.use(cors({origin:true}));
+	app.use(express.urlencoded({ extended: false }));
+
+    app.listen(PORT);
+    console.log(`Connected to port ${PORT}`);
+
+    const limiter = rateLimit({
+		windowMs: 1 * 60 * 1000, // 1 minutes
+		max: 30,				// max 30 requests
+		standardHeaders: true,
+		legacyHeaders: false
+	});
+	app.use(limiter);
+
+	// Global Error Handling
+	app.use(errorMiddleware);
+
+    
+    const translateText = async (req,res,next) => {
+        try {
+            const{text,target} = req.body;
+            if(!text || !target){
+               return next(new ErrorHandler(406,"text and target are required"));
+            }
+            let translations = await translate.translate(text, target);
+
+            const request = {
+                input: {text: translations[0]},
+                // Select the language and SSML voice gender (optional)
+                voice: {languageCode: 'hi-IN', ssmlGender: 'NEUTRAL'},
+                // select the type of audio encoding
+                audioConfig: {audioEncoding: 'MP3'},
+              };
+              const [response] = await client.synthesizeSpeech(request);
+
+
+  return res.status(200).json({success:true, textdata:translations[0],audiodata:response});
+} catch (err) {
+    next(err);
+}
+}
+
+app.use(express.static('dist'));
+
+app.get('*', (req, res) => {
+res.sendFile(path.resolve(__dirname, 'dist', 'index.html'));
+});
+
+app.post('/translate',translateText,errorMiddleware);
+
+
