9b00a4ecce9d3e0373abdaed2a607b3c3db5dc06
Archiehere <cheeku67@gmail.com>
2023-06-28 19:33:28 +0530

    FIX:Error

--- index.js
+++ index.js
@@ -67,7 +67,7 @@ const app = express();
 }
 
 app.use(express.static('dist'));
-
+const path = require('path');
 app.get('*', (req, res) => {
 res.sendFile(path.resolve(__dirname, 'dist', 'index.html'));
 });
